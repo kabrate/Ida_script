f97b8746b6821b05872dadef42bbf34808b44020
kabrate <937160666@qq.com>
2021-03-28 22:47:04 +0800

    添加项目文件。

---
+++ Ida_script/data_byte.py
@@ -0,0 +1,11 @@
+import struct
+import binascii
+start = AskAddr(0x14028E330 , "start address:")
+print ("start address:%08x"%(start))
+length = AskLong(0x400 , "length:")
+print ("length:%d"%(length))
+fn = AskStr("c:\\table.dump" ,"save as:" )
+with open(fn,"wb+") as f:
+    for addr in range(start , start+length,8):
+        f.write( '0x'+binascii.b2a_hex(struct.pack("B",Byte(addr)))+',')#Byte
+print ("success to save as ",fn)
