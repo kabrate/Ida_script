391de7314e8bef3d6eb22971135877b711266abe
kabrate <937160666@qq.com>
2021-03-30 09:44:18 +0800

    add big endian small endian

--- Ida_script/data_byte.py
+++ Ida_script/data_byte.py
@@ -7,5 +7,5 @@ print ("length:%d"%(length))
 fn = AskStr("c:\\table.dump" ,"save as:" )
 with open(fn,"wb+") as f:
     for addr in range(start , start+length):
-        f.write( '0x'+binascii.b2a_hex(struct.pack("B",Byte(addr)))+',')#Byte
+        f.write( '0x'+binascii.b2a_hex(struct.pack("B",Byte(addr)))+',')#Byte  >big endian 大端  < 小端 small endian 
 print ("success to save as ",fn)
--- Ida_script/data_word.py
+++ Ida_script/data_word.py
@@ -7,5 +7,5 @@ print ("length:%d"%(length))
 fn = AskStr("c:\\table.dump" ,"save as:" )
 with open(fn,"wb+") as f:
     for addr in range(start , start+length,2):
-        f.write( '0x'+binascii.b2a_hex(struct.pack("H",Word(addr)))+',')//word
+        f.write( '0x'+binascii.b2a_hex(struct.pack("H",Word(addr)))+',')#word
 print ("success to save as ",fn)
